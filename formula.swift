e0fc072aa9d8234c3ceab0e8be5eefeca9d65acd
deathlezz <78265796+deathlezz@users.noreply.github.com>
2021-10-23 18:38:50 +0200

    Update formula.swift

--- formula.swift
+++ formula.swift
@@ -4,13 +4,18 @@
 //  Created by deathlezz on 22/10/2021.
 //
 
+// amount of names you want to pick
 let namesAmount = 5
+// array of entered names to pick
 let namesArray = ["Jack", "Josh", "Jerry", "Rick", "Sue"]
+// array of picked and unique names
 var newNamesArray: [String] = []
 
 while newNamesArray.count != namesAmount {
+    // pick random name from array
     let random = namesArray.randomElement()
     
+    // add only unique name to the new array
     if !newNamesArray.contains(random!) {
         newNamesArray.append(random!)
     }
